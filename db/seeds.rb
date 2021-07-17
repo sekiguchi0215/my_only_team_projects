618667dcc609508daef1df19832dd8b3cf753918
sekiguchi0215 <dsphere5296@gmail.com>
2021-07-18 00:31:06 +0900

    texts テーブルにデータを投入する処理を追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "csv"
+
 
 %w[texts movies].each do |table_name|
   ActiveRecord::Base.connection.execute("TRUNCATE TABLE #{table_name} RESTART IDENTITY CASCADE")
+end
+
+CSV.foreach("db/csv_data/text_data.csv", headers: true) do |row|
+  Text.create(
+    genre: row["genre"],
+    title: row["title"],
+    content: row["content"]
+  )
 end
 
 email = "test@example.com"
