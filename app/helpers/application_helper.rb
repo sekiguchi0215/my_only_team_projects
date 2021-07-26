9a4cc5ff210bef2e859f9540458e1056362ad375
sekiguchi0215 <dsphere5296@gmail.com>
2021-07-26 23:03:44 +0900

    application_helper.rb の記述を変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,9 +2,8 @@ module ApplicationHelper
   def max_width
     if controller_name == "texts" && action_name == "show"
       "mw-md"
-    # Devise 導入後に有効化
-    # elsif
-    #  "mw-sm"
+    elsif devise_controller?
+      "mw-sm"
     else
       "mw-xl"
     end
