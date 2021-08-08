ad2dd732a1e883bf3bd0bdb1ae556fd18baf604c
sekiguchi0215 <dsphere5296@gmail.com>
2021-08-08 23:33:59 +0900

    クエリパラメータで表示を分岐するように、コントローラを編集

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -1,6 +1,10 @@
 class TextsController < ApplicationController
   def index
-    @texts = Text.where(genre: Text::RAILS_GENRE_LIST)
+    if params[:genre] == "php"
+      @texts = Text.where(genre: Text::PHP_GENRE_LIST)
+    else
+      @texts = Text.where(genre: Text::RAILS_GENRE_LIST)
+    end
   end
 
   def show
