f270aeb4298948760d5894b524f89bcf5f10a4b3
sekiguchi0215 <dsphere5296@gmail.com>
2021-08-09 23:12:41 +0900

    各モデルに、バリデーションとアソシエーションを記述

--- app/models/text.rb
+++ app/models/text.rb
@@ -5,13 +5,15 @@ class Text < ApplicationRecord
     validates :content
   end
 
+  has_many :read_progresses, dependent: :destroy
+
   enum genre: {
     invisible: 0,
     basic: 1,
     git: 2,
     ruby: 3,
     rails: 4,
-    php: 5
+    php: 5,
   }
 
   RAILS_GENRE_LIST = %w[basic git ruby rails].freeze
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :rememberable, :validatable
 
+  has_many :read_progresses, dependent: :destroy
+
   def self.guest
     find_or_create_by!(email: "guest@example.com") do |user|
       user.password = SecureRandom.urlsafe_base64
